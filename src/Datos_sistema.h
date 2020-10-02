98ab549d3bcadc4838ebba3525c89149cce22113
wolam <wilhelm@estudiantec.cr>
2020-10-01 20:56:19 -0600

    Funcionalidad previa de estado de vuelo.

--- src/Datos_sistema.h
+++ src/Datos_sistema.h
@@ -8,67 +8,75 @@
 #define CONTR "wil_bd_psswrd"
 #define SRVR "localhost"
 
-
+//Llamadas a funcionalidades de la bd
 #define ESTADISTICAS_VENTAS "CALL estadistica_ventas"
 #define ESTADISTICAS_RESERVACIONES "CALL estadistica_personas"
 
+#define CONSULTA_VUELO "CALL consVuelo("
+#define CONSULTA_COSTO_ASIENTOS "CALL costo_asiento("
+#define CONSULTA_POS_ASIENTOS "CALL fila_asiento("
+#define CONSULTA_CANT_ASIENTOS "CALL cant_asientos("
+#define CONSULTA_INFO_RESRV_POR_VUELO "CALL reservacion("
+//#define CONSULTA_INFO_RESRV_POR_ID "CALL reservacion("
+#define CONSULTA_COSTO_RESRV_POR_VUELO "CALL monto_reservacion("
 
 MYSQL *conexion;
 MYSQL_RES *resultado;
 MYSQL_ROW reg;
 
-void
-cerrar_conexion()
+void cerrar_conexion()
 {
-    //Terminar resultados y conexion
-	mysql_free_result(resultado);
+	//Terminar resultados y conexion
 	mysql_close(conexion);
 	exit(SALIDA_EXITOSA);
-
 }
-
 
 void realizar_conexion()
 {
-    conexion = mysql_init(NULL);
-    
-    if (!mysql_real_connect(conexion, SRVR, USR_DBA, 
-                                CONTR, NMB_BD, 0, NULL, 0)) {
-		fprintf(stderr,ERROR_CONEXION "%s\n", mysql_error(conexion));
-        cerrar_conexion();
+	conexion = mysql_init(NULL);
+
+	if (!mysql_real_connect(conexion, SRVR, USR_DBA,
+							CONTR, NMB_BD, 0, NULL, 0))
+	{
+		fprintf(stderr, ERROR_CONEXION "%s\n", mysql_error(conexion));
+		cerrar_conexion();
 		exit(TIEMPO_EJECUCION);
 	}
-
 }
 
-int realizar_consulta(char* consulta)
+int realizar_consulta(char *consulta)
 {
 
-    /* enviar consulta SQL */
-	if (mysql_query(conexion, consulta)){
+	/* enviar consulta SQL */
+	if (mysql_query(conexion, consulta))
+	{
 		//fprintf(stderr, ERROR_CONSULTA "%s\n", mysql_error(conexion));
-		if (!strncmp(mysql_error(conexion),ERROR_DUP,strlen(ERROR_DUP)))
+		if (!strncmp(mysql_error(conexion), ERROR_DUP, strlen(ERROR_DUP)))
 			return COD_ERROR_DUP;
-		else{
+		else
+		{
 			// error de sintaxis de consulta
 			return COD_ERROR_SINTX;
 		}
 	}
-	else{
+	else
+	{
 		return SALIDA_EXITOSA;
 	}
 }
 
-
 void mostrar_estadisticas_ventas()
 {
-	if (realizar_consulta(ESTADISTICAS_VENTAS)==1){
+	if (realizar_consulta(ESTADISTICAS_VENTAS) == 1)
+	{
 		resultado = mysql_store_result(conexion);
-		while ((reg = mysql_fetch_row(resultado)) != NULL){
+		while ((reg = mysql_fetch_row(resultado)) != NULL)
+		{
 			printf("VUELOS MAS VENDIDOS\n\nCOD.VUELO [%s] VENTAS: %s\n\n", reg[0], reg[1]);
 		}
-	
-	} else{
+	}
+	else
+	{
 		printf(ERROR_CONSULTA "[ Verificar estadisticas de ventas ] \n");
 		//mysql_free_result(resultado);
 	}
@@ -77,14 +85,105 @@ void mostrar_estadisticas_ventas()
 
 void mostrar_estadisticas_reservaciones()
 {
-	if (realizar_consulta(ESTADISTICAS_RESERVACIONES)== 1){
+	if (realizar_consulta(ESTADISTICAS_RESERVACIONES) == 1)
+	{
 		resultado = mysql_store_result(conexion);
-		while ((reg = mysql_fetch_row(resultado)) != NULL){
+		while ((reg = mysql_fetch_row(resultado)) != NULL)
+		{
 			printf("VUELOS MAS RESERVADOS\n\nCOD.VUELO [%s] RESERVACIONES: %s\n\n", reg[0], reg[1]);
 		}
-	}else{
+	}
+	else
+	{
 		printf(ERROR_CONSULTA "[ Verificar estadisticas de reservaciones ] \n");
 	}
+	//mysql_free_result(resultado);
+	mysql_next_result(conexion);
+}
+
+char* formatear_cons_vuelo(char* consulta,char *id_vuelo)
+{
+	char* cons = calloc(TAM_CONSULTA,sizeof(char));
+	strcpy(cons,consulta);
+    strcat(cons,id_vuelo);
+    strcat(cons,")");
+    return cons;
+}
+
+void mostrar_info_vuelo(char *id_vuelo)
+{
+
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_VUELO, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	if (!mysql_num_rows(resultado))
+		printf("<VUELO NO ENCONTRADO>\n");
+
+	else
+	{
+		while ((reg = mysql_fetch_row(resultado)) != NULL)
+		{
+			printf("MOSTRANDO INFO VUELO\n");
+		}
+	}
+
 	mysql_free_result(resultado);
 	mysql_next_result(conexion);
+}
+
+void mostrar_asientos_en_vuelo(char *id_vuelo)
+{
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_COSTO_ASIENTOS, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	while ((reg = mysql_fetch_row(resultado)) != NULL)
+	{
+		printf("MOSTRANDO INFO ASIENTOS\n");
+	}
+
+	mysql_free_result(resultado);
+	mysql_next_result(conexion);
+
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_POS_ASIENTOS, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	while ((reg = mysql_fetch_row(resultado)) != NULL)
+	{
+		printf("MOSTRANDO POS ASIENTOS\n");
+	}
+
+	mysql_free_result(resultado);
+	mysql_next_result(conexion);
+
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_CANT_ASIENTOS, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	while ((reg = mysql_fetch_row(resultado)) != NULL)
+	{
+		printf("MOSTRANDO CANTIDAD ASIENTOS\n");
+	}
+
+	mysql_free_result(resultado);
+	mysql_next_result(conexion);
+}
+
+void mostrar_reservaciones_en_vuelo(char *id_vuelo)
+{
+
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_INFO_RESRV_POR_VUELO, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	while ((reg = mysql_fetch_row(resultado)) != NULL)
+	{
+		printf("MOSTRANDO INFO RESERVA\n");
+	}
+
+	mysql_free_result(resultado);
+	mysql_next_result(conexion);
+
+	realizar_consulta(formatear_cons_vuelo(CONSULTA_COSTO_RESRV_POR_VUELO, id_vuelo));
+	resultado = mysql_store_result(conexion);
+	while ((reg = mysql_fetch_row(resultado)) != NULL)
+	{
+		printf("MOSTRANDO COSTO RESERVA\n");
+	}
+
+	mysql_free_result(resultado);
+	mysql_next_result(conexion);
+
 }
--- src/Sistema_vuelos.c
+++ src/Sistema_vuelos.c
@@ -1,34 +1,35 @@
 #include <stdlib.h>
 #include "Datos_sistema.h"
 
-FILE* abrir_archivo(char* ruta,char* modo);
-void cargar_usuarios (char* nombre_archivo);
-int insertar_usuario(char* usuario);
-char* formatear_fecha(char* fecha);
-char* formatear_usuario(char* usuario);
-char* obtener_reporte(int res_consulta);
-char *especificar_ruta();
-char solicitar_opcion();
-void mostrar_menu(char* tipo_menu);
+FILE *abrir_archivo(char *ruta, char *modo);
+int insertar_usuario(char *usuario);
+char *formatear_fecha(char *fecha);
+char *formatear_usuario(char *usuario);
+char *obtener_reporte(int res_consulta);
+char *pedir_str_input(char *msj);
+char pedir_caracter_input(char *msj);
+void mostrar_menu(char *tipo_menu);
 void ejecutar_opcion_menu_principal();
 void ejecutar_opcion_submenu_general();
 void ejecutar_opcion_submenu_operativo();
+void cargar_usuarios(char *nombre_archivo);
+void estado_vuelo();
 
-int
-main()
+int main()
 {
-   realizar_conexion();
-   ejecutar_opcion_menu_principal();
-   return 0;
+    realizar_conexion();
+    ejecutar_opcion_menu_principal();
+    return 0;
 }
 
-
-void
-ejecutar_opcion_menu_principal()
-{   
+void ejecutar_opcion_menu_principal()
+{
     mostrar_menu("principal");
-    do { char opcion = solicitar_opcion();
-        switch (opcion){
+    do
+    {
+        char opcion = pedir_caracter_input("<OPCION DESEADA> ");
+        switch (opcion)
+        {
         case OPCIONES_OPERATIVAS:
             ejecutar_opcion_submenu_operativo();
             break;
@@ -40,17 +41,19 @@ ejecutar_opcion_menu_principal()
         default:
             printf(ERROR_OPCION);
             break;
-    }}while(TIEMPO_EJECUCION);
+        }
+    } while (TIEMPO_EJECUCION);
 }
 
-
-void
-ejecutar_opcion_submenu_general()
+void ejecutar_opcion_submenu_general()
 {
     mostrar_menu("general");
     char opcion = '\0';
-    do { opcion = solicitar_opcion();
-     switch (opcion){
+    do
+    {
+        opcion = pedir_caracter_input("<OPCION DESEADA> ");
+        switch (opcion)
+        {
         case OPCION_CAMBIO_SUBMENU:
             ejecutar_opcion_submenu_operativo();
             break;
@@ -68,14 +71,18 @@ ejecutar_opcion_submenu_general()
         default:
             printf(ERROR_OPCION);
             break;
-    }} while(opcion != OPCION_SALIR_GENERAL);
+        }
+    } while (opcion != OPCION_SALIR_GENERAL);
 }
-void
-ejecutar_opcion_submenu_operativo()
-{   
+void ejecutar_opcion_submenu_operativo()
+{
     mostrar_menu("operativo");
     char opcion = '\0';
-    do { opcion = solicitar_opcion(); switch (opcion){
+    do
+    {
+        opcion = pedir_caracter_input("<OPCION DESEADA> ");
+        switch (opcion)
+        {
         case OPCION_CAMBIO_SUBMENU:
             ejecutar_opcion_submenu_general();
             break;
@@ -86,10 +93,10 @@ ejecutar_opcion_submenu_operativo()
             printf("3\n");
             break;
         case OPCION_CARGAR_USRIO:
-            cargar_usuarios(especificar_ruta());
+            cargar_usuarios(pedir_str_input("<NOMBRE DEL ARCHIVO> "));
             break;
         case OPCION_ESTADO_VLO:
-            printf("5\n");
+            estado_vuelo();
             break;
         case OPCION_ESTADISTICAS:
             mostrar_estadisticas_reservaciones();
@@ -99,124 +106,133 @@ ejecutar_opcion_submenu_operativo()
             cerrar_conexion();
         default:
             printf(ERROR_OPCION);
-    }}while(opcion != OPCION_SALIR_OPERATIVO);
+        }
+    } while (opcion != OPCION_SALIR_OPERATIVO);
 }
 
-
-void 
-mostrar_menu(char* tipo_menu)
+void mostrar_menu(char *tipo_menu)
 {
     char abrir_menu[20] = "cat menus/menu_";
-    strcat(abrir_menu,tipo_menu);
+    strcat(abrir_menu, tipo_menu);
     system(abrir_menu);
 }
 
-char
-solicitar_opcion()
+char pedir_caracter_input(char* msj)
 {
     char opcion;
-    printf("\n <OPCION A ELEGIR> ");
-    scanf(" %c",&opcion);
+    printf("%s",msj);
+    scanf(" %c", &opcion);
     return opcion;
 }
 
-
 FILE *
-abrir_archivo(char* ruta,char* modo)
+abrir_archivo(char *ruta, char *modo)
 {
     FILE *ref_archivo;
-    ref_archivo = fopen(ruta,modo);
+    ref_archivo = fopen(ruta, modo);
     if (!ref_archivo)
     {
-        printf("%s %s\n", ERROR_ARCHIVO,ruta);
+        printf("%s %s\n", ERROR_ARCHIVO, ruta);
         return NULL;
-    
-    }return ref_archivo;
-   
+    }
+    return ref_archivo;
 }
 
-
-void 
-cargar_usuarios
-(char* nombre_archivo)
+void cargar_usuarios(char *nombre_archivo)
 {
 
     char ruta_archivo[TAM_RUTA] = {"usuarios/"};
-    strcat(ruta_archivo,nombre_archivo); 
+    strcat(ruta_archivo, nombre_archivo);
     FILE *ref_archivo_usuarios;
-    ref_archivo_usuarios = abrir_archivo(ruta_archivo,"r");
+    ref_archivo_usuarios = abrir_archivo(ruta_archivo, "r");
 
-    if (ref_archivo_usuarios != NULL){
-        char usuario [TAM_USUARIO];
+    if (ref_archivo_usuarios != NULL)
+    {
+        char usuario[TAM_USUARIO];
         char ruta_reporte[TAM_RUTA] = {"reportes/reporte_"};
-        strcat(ruta_reporte,nombre_archivo);
-        FILE* ref_archivo_reporte = abrir_archivo(ruta_reporte,"w+");
+
+        strcat(ruta_reporte, nombre_archivo);
+        FILE *ref_archivo_reporte = abrir_archivo(ruta_reporte, "w+");
         while (fgets(usuario, TAM_USUARIO, ref_archivo_usuarios))
         {
             if (usuario[strlen(usuario) - 1] == '\n')
-                usuario[strlen(usuario)-1] = '\0';
+                usuario[strlen(usuario) - 1] = '\0';
 
-            fseek(ref_archivo_reporte,0,SEEK_END);
+            fseek(ref_archivo_reporte, 0, SEEK_END);
             fputs(obtener_reporte(insertar_usuario(usuario)), ref_archivo_reporte);
         }
         fclose(ref_archivo_reporte);
         fclose(ref_archivo_usuarios);
-    }else{
+    }
+    else
+    {
         printf("\n<ASEGURESE QUE EL ARCHIVO EXISTA>\n");
         return;
     }
 }
 
-int
-insertar_usuario(char* usuario)
+int insertar_usuario(char *usuario)
 {
     char cons[TAM_CONSULTA] = "INSERT INTO usuario VALUES(";
-    char* usuario_formateado = formatear_usuario(usuario); 
-    char* fecha  = formatear_fecha(strcat(strrchr(usuario, ','),",'%Y-%m-%d'))"));
-    strcat(cons,usuario_formateado);
-    strcat(cons,fecha);
+    char *usuario_formateado = formatear_usuario(usuario);
+    char *fecha_formateada = formatear_fecha(strcat(strrchr(usuario, ','), ",'%Y-%m-%d'))"));
+    strcat(cons, usuario_formateado);
+    strcat(cons, fecha_formateada);
     return realizar_consulta(cons);
 }
 
-char*
+char *
 obtener_reporte(int res_consulta)
 {
-    char * reporte = calloc(13,sizeof(char));
-    if(!res_consulta)
-        strcpy(reporte,"AGREGADO\n");
-    else if(res_consulta==COD_ERROR_SINTX)
-        strcpy(reporte,"ERROR\n");
-    else{
-        strcpy(reporte,"DUPLICADO\n");
+    char *reporte = calloc(13, sizeof(char));
+    if (!res_consulta)
+        strcpy(reporte, "AGREGADO\n");
+    else if (res_consulta == COD_ERROR_SINTX)
+        strcpy(reporte, "ERROR\n");
+    else
+    {
+        strcpy(reporte, "DUPLICADO\n");
     }
     return reporte;
 }
 
-
-char* formatear_fecha(char* fecha)
+char *formatear_fecha(char *fecha)
 {
-    char* func_fecha  = calloc(TAM_FECHA,sizeof(char));
-    strcpy(func_fecha,"STR_TO_DATE(");
+    char *func_fecha = calloc(TAM_FECHA, sizeof(char));
+    strcpy(func_fecha, "STR_TO_DATE(");
     fecha[0] = ' ';
-    strcat(func_fecha,fecha);
+    strcat(func_fecha, fecha);
     return func_fecha;
 }
 
-char* formatear_usuario(char* usuario)
+char *formatear_usuario(char *usuario)
 {
-    char* usuario_formateado = calloc(TAM_USUARIO,sizeof(char));
-    strncpy(usuario_formateado,usuario,strlen(usuario)-12);
+    char *usuario_formateado = calloc(TAM_USUARIO, sizeof(char));
+    strncpy(usuario_formateado, usuario, strlen(usuario) - 12);
     return usuario_formateado;
 }
 
-
-char *especificar_ruta()
+char *pedir_str_input(char* msj)
 {
     getchar();
-    char* buffer = malloc(TAM_RUTA*sizeof(char));
-    printf("Digite el nombre del archivo -> ");
+    char *buffer = malloc(TAM_RUTA * sizeof(char));
+    printf("%s",msj);
     fgets(buffer, TAM_RUTA, stdin);
-    if ((strlen(buffer) > 0) && (buffer[strlen (buffer) - 1] == '\n'))
-        buffer[strlen (buffer) - 1] = '\0';
+    if ((strlen(buffer) > 0) && (buffer[strlen(buffer) - 1] == '\n'))
+        buffer[strlen(buffer) - 1] = '\0';
     return buffer;
 }
+
+void estado_vuelo()
+{
+    char id_vuelo[16];
+    strcpy(id_vuelo,pedir_str_input("<ID DEL VUELO> "));
+
+    mostrar_info_vuelo(id_vuelo);
+
+    mostrar_asientos_en_vuelo(id_vuelo);
+
+    mostrar_reservaciones_en_vuelo(id_vuelo);
+
+}
+
